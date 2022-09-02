3ab5a6a08bd04eb2842944d25382da6a8fb3f2a2
Purplik <60230933+PurplikDev@users.noreply.github.com>
2022-09-02 11:42:10 +0200

    shamblingHusk attack animation working

--- Assets/Scripts/EntitiesStats/entityAttack.cs
+++ Assets/Scripts/EntitiesStats/entityAttack.cs
@@ -16,8 +16,20 @@ public class entityAttack : MonoBehaviour
     {
         RaycastHit hit;
         Physics.Raycast(aimPoint.position, aimPoint.forward, out hit, 10f);
-        targetStats = hit.transform.GetComponent<EntityStats>();
-        targetStats.Damage(10, "true");
-        Debug.Log(hit.transform.name + " " + targetStats.health.ToString());
+        
+        if (hit.transform != null) {
+            targetStats = hit.transform.GetComponent<EntityStats>();
+            targetStats.Damage(10, "true");
+            Debug.Log(hit.transform.name + " " + targetStats.health.ToString());
+        }
+    }
+
+    void Update() {
+        RaycastHit hit;
+        Physics.Raycast(aimPoint.position, aimPoint.forward, out hit, 5f);
+
+        if(hit.transform.CompareTag("Player")) {
+            transform.GetComponent<Animator>().SetTrigger("attacked");
+        }
     }
 }
