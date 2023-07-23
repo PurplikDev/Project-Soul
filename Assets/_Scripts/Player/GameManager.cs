39ac23735ed417e29ffac3c1b01072684270667e
PurplikDev <vojta.g@atlas.cz>
2023-07-23 02:06:51 +0200

    Rewrite Setup + Movement

---
+++ Assets/_Scripts/Player/GameManager.cs
@@ -0,0 +1,44 @@
+using Purplik.PixelRoguelike.Input;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private InputReader _input;
+    [SerializeField] private GameObject pauseMenu;
+
+    private void Start()
+    {
+        // Gameplay UI Events
+        _input.PauseEvent += HandlePause;
+        _input.InventoryEvent += HandleInventory;
+
+        // UI Events
+        _input.CloseUIEvent += CloseAllUI;
+        _input.CloseInvetoryEvent += CloseInventory;
+    }
+
+    private void HandlePause()
+    {
+        pauseMenu.SetActive(true);
+    }
+
+    // TODO: Correct implementation, current one is only for testing
+    private void HandleInventory()
+    {
+        pauseMenu.SetActive(true);
+    }
+
+    // TODO: Correct implementation, current one is only for testing
+    private void CloseAllUI()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    // TODO: Correct implementation, current one is only for testing
+    private void CloseInventory()
+    {
+        pauseMenu.SetActive(false);
+    }
+}
---
+++ Assets/_Scripts/Player/PlayerAim.cs
@@ -0,0 +1,18 @@
+using Purplik.PixelRoguelike.Input;
+using UnityEngine;
+
+public class PlayerAim : MonoBehaviour
+{
+    private Camera cam;
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        
+    }
+
+}
