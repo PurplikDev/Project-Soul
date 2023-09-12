79a99afe1a558c8924674e8c931fd79445690a1b
Purplik <60230933+PurplikDev@users.noreply.github.com>
2023-09-12 11:57:10 +0200

    :3

--- Assets/_Scripts/enviroment/Entity/Player/Inventory/Inventory.cs
+++ Assets/_Scripts/enviroment/Entity/Player/Inventory/Inventory.cs
@@ -6,8 +6,8 @@ namespace roguelike.enviroment.entity.player.inventory {
     [Serializable]
     public class Inventory {
 
-        public NonNullList<ItemStack> Items = new NonNullList<ItemStack>(32, ItemStack.EMPTY);
-        public NonNullList<ItemStack> Equipment = new NonNullList<ItemStack>(32, ItemStack.EMPTY);
+        public NonNullList<ItemStack> Items = new NonNullList<ItemStack>(24, ItemStack.EMPTY);
+        public NonNullList<ItemStack> Equipment = new NonNullList<ItemStack>(6, ItemStack.EMPTY);
         public NonNullList<ItemStack> Trinkets = new NonNullList<ItemStack>(32, ItemStack.EMPTY);
 
 
