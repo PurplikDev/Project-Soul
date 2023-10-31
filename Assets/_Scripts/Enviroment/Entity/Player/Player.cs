6249e809222242d2864aa9d2ff8d790c080e27ee
PurplikDev <vojta.g@atlas.cz>
2023-10-31 05:35:14 +0100

    input fix

--- Assets/_Scripts/Enviroment/Entity/Player/Player.cs
+++ Assets/_Scripts/Enviroment/Entity/Player/Player.cs
@@ -1,16 +1,19 @@
 using roguelike.core.item;
 using roguelike.enviroment.entity.player.StateMachine;
 using roguelike.system.input;
+using System.Linq;
+using UnityEngine;
 
 namespace roguelike.enviroment.entity.player {
     public class Player : Entity {
-        public InputReader inputReader;
+        public InputReader InputReader;
         public Inventory PlayerInventory;
         public PlayerStateMachine PlayerStateMachine;
 
         private void Awake() {
+            this.InputReader = Resources.LoadAll<InputReader>("data/player").First();
             PlayerInventory = new Inventory(this);
-            PlayerStateMachine = new PlayerStateMachine(this, inputReader);
+            PlayerStateMachine = new PlayerStateMachine(this, InputReader);
         }
 
         protected override void Update() {
