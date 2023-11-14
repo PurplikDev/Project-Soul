cdffd76e0099fecfe1490cacdeba8bfc93347bb4
Purplik <60230933+PurplikDev@users.noreply.github.com>
2023-11-14 13:06:57 +0100

    improved and rewritten generator

--- Assets/_Scripts/core/managers/DungeonManager.cs
+++ Assets/_Scripts/core/managers/DungeonManager.cs
@@ -1,3 +1,4 @@
+using roguelike.core.utils;
 using roguelike.system.singleton;
 using System;
 using System.Collections.Generic;
@@ -6,94 +7,116 @@ using System.Text;
 using UnityEngine;
 using static roguelike.system.manager.DungeonManager.Room;
 using Random = UnityEngine.Random;
+using static roguelike.core.utils.DirectionUtils;
 
 namespace roguelike.system.manager {
     public class DungeonManager : Singleton<DungeonManager> {
 
-        public Room[,] dungeon;
+        public Transform platform;
+        public Transform cube;
+
+        public Room[,] dungeon; // [y,x] WHYYYY?!?!? why isn't it [x,y]?????
         public List<Room> rooms;
         public List<Room> generatedRooms;
+        public List<Room> sideRooms;
 
+        public List<Room> ALLROOMS = new List<Room>();
+
+        private DungeonLength length = DungeonLength.VERYLONG;
 
         protected override void Awake() {
-            GenerateMain();
-            LogDungeon();
+            for(int i = 0; i < 5; i++) {
+                do {
+                    GenerateMain();
+                } while(dungeon == null);
+                LogDungeon();
+            }
+
             base.Awake();
         }
 
         public void GenerateMain() {
-            int size = 9;
-            int limit = 0;
+            int size = (int)length;
+            int limit = size / 2;
+
+            int failsafe = 0;
+            Room possibleRoom = null;
 
             dungeon = new Room[size, size];
             rooms = new List<Room>();
             generatedRooms = new List<Room>();
 
-            for (int i = 0; i < 9; i++) {
-                for (int j = 0; j < 9; j++) {
+            for (int i = 0; i < (int)length; i++) {
+                for (int j = 0; j < (int)length; j++) {
                     dungeon[i, j] = new Room(RoomType.EMPTY, i, j);
                 }
             }
 
-            var starterRoom = new Room(RoomType.STARTER, 4, 4);
-            dungeon[4, 4] = starterRoom;
-            rooms.Add(starterRoom);
+            GenerateRoom(Random.Range(2, size - 2), Random.Range(2, size - 2), RoomType.STARTER, false);
 
             while(rooms.Count > 0) {
                 var currentRoom = rooms.First();
                 rooms.Remove(currentRoom);
-
-                Room neighbor = null;
                 bool reroll = false;
-                do {
-                    reroll = false;
-                    try {
-                        switch ((Direction)Random.Range(0, 3)) {
-                            case Direction.UP:
-                                neighbor = dungeon[currentRoom.x, currentRoom.y + 1];
-                                break;
 
-                            case Direction.DOWN:
-                                neighbor = dungeon[currentRoom.x, currentRoom.y - 1];
-                                break;
-
-                            case Direction.LEFT:
-                                neighbor = dungeon[currentRoom.x - 1, currentRoom.y];
-                                break;
-
-                            case Direction.RIGHT:
-                                neighbor = dungeon[currentRoom.x + 1, currentRoom.y];
-                                break;
-                        }
-                    } catch(IndexOutOfRangeException) {
-                        reroll = true;
-                        continue; // ?\_(?)_/?
+                if(rooms.Count > limit) {
+                    if(Random.Range(0, 10) > rooms.Count - limit) {
+                        Debug.LogWarning("done xd");
+                        break;
                     }
-                } while (reroll || neighbor.Type != 0);
+                }
+                do {
+                    try {
+                        possibleRoom = GetRelativeRoom(currentRoom.y, currentRoom.x, (Direction) Random.Range(0, 4));
+                    } catch(IndexOutOfRangeException) {
+                        possibleRoom = null;
+                        reroll = true;
+                        failsafe++;
+                        if(failsafe > 75) {
+                            break;
+                        }
+                    }
+                } while(reroll || possibleRoom.Type != RoomType.EMPTY);
 
-                if (limit < 9) {
-                    var newRoom = new Room(RoomType.NORMAL, neighbor.x, neighbor.y);
-                    dungeon[neighbor.x, neighbor.y] = newRoom;
-                    rooms.Add(newRoom);
-                    generatedRooms.Add(newRoom);
-                    limit++;
-                } else {
-                    dungeon[neighbor.x, neighbor.y] = new Room(RoomType.FINAL, neighbor.x, neighbor.y);
+                if(possibleRoom == null) {
+                    Debug.LogWarning("possible room was null");
+                    break;
+                }
+
+                if(possibleRoom.Type == RoomType.EMPTY) {
+                    GenerateRoom(possibleRoom.y, possibleRoom.x, RoomType.NORMAL, false);
                 }
             }
         }
 
-        public void GenerateSide() {
-            // todo: add generation of side rooms
-            // don't forget about doors maggot
+        public void GenerateRoom(int y, int x, RoomType type, bool isSpecial) {
+            var room = new Room(type, y, x);
+            dungeon[y, x] = room;
+            if(!isSpecial) {
+                rooms.Add(room);
+            }
+            generatedRooms.Add(room);
+        }
+
+        public Room GetRelativeRoom(int y, int x, Direction direction) {
+            switch(direction) {
+                case Direction.UP:
+                    return dungeon[y - 1, x];
+                case Direction.DOWN:
+                    return dungeon[y + 1, x];
+                case Direction.LEFT:
+                    return dungeon[y, x - 1];
+                default: // technically this is Direction.RIGHT, i just wanted the warning to shut up
+                    return dungeon[y, x + 1];
+            }
         }
 
         private void LogDungeon() {
             StringBuilder builder = new StringBuilder();
 
-            for (int i = 0; i < 9; i++) {
+            for (int i = 0; i < (int)length; i++) {
                 builder.Append("|");
-                for (int j = 0; j < 9; j++) {
+                for (int j = 0; j < (int)length; j++) {
                     builder.Append("[" + dungeon[i, j].ToString() + "]");
                 }
                 builder.AppendLine("|");
@@ -101,29 +124,30 @@ namespace roguelike.system.manager {
             Debug.Log(builder);
         }
 
+
         public enum DungeonHazard {
 
         }
 
         public enum DungeonLength {
-            SHORT,
-            MEDIUM,
-            LONG,
-            VERYLONG
+            SHORT = 5,
+            MEDIUM = 9,
+            LONG = 11,
+            VERYLONG = 15
         }
 
         public class Room {
             public RoomType Type;
 
-            public Direction[] doors;
+            public List<Direction> walls = new List<Direction>();
 
             public int x, y;
 
-            public Room(RoomType type, int x, int y) {
+            public Room(RoomType type, int y, int x) {
                 Type = type;
 
-                this.x = x; 
                 this.y = y;
+                this.x = x; 
             }
 
             public override string ToString() {
@@ -134,14 +158,8 @@ namespace roguelike.system.manager {
                 EMPTY = 0,
                 STARTER = 1,
                 NORMAL = 2,
-                FINAL = 3
-            }
-
-            public enum Direction {
-                UP = 0,
-                DOWN = 1,
-                LEFT = 2,
-                RIGHT = 3
+                FINAL = 3,
+                SPECIAL = 4
             }
         }
     }
---
+++ Assets/_Scripts/core/utils/DirectionUtils.cs
@@ -0,0 +1,20 @@
+namespace roguelike.core.utils {
+    public static class DirectionUtils {
+        public static Direction getOpposite(Direction direction) {
+            switch(direction) { // i hate this piece of code, too lazy to think of a proper solution
+                case Direction.UP: return Direction.DOWN;
+                case Direction.DOWN: return Direction.UP;
+                case Direction.LEFT: return Direction.RIGHT;
+                case Direction.RIGHT: return Direction.LEFT;
+            }
+            return Direction.UP;
+        }
+
+        public enum Direction {
+            UP = 0,
+            DOWN = 1,
+            LEFT = 2,
+            RIGHT = 3
+        }
+    }
+}
