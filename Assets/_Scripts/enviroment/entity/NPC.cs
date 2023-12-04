bf6ee934947ce56ea28c00f5ab38aaf2f2eb3b99
PurplikDev <vojta.g@atlas.cz>
2023-12-04 05:43:05 +0100

    file organization
    was lazy, moved files to their proper place

--- Assets/_Scripts/enviroment/entity/NPC.cs
+++ Assets/_Scripts/enviroment/entity/NPC.cs
@@ -1,10 +1,11 @@
 using roguelike.enviroment.entity.player;
 using roguelike.enviroment.world;
+using UnityEngine;
 
 namespace roguelike.enviroment.entity.npc {
     public class NPC : Entity, IInteractable {
         public void Interact(Player player) {
-            
+            Debug.Log("Testing");
         }
     }
 }
