0c23b812918c368f56ddbe529280cdbdffce0116
PurplikDev <vojta.g@atlas.cz>
2023-12-10 22:01:29 +0100

    Revert "Delete Assets/_Scripts/Enviroment directory"
    This reverts commit 4aa8bd1ff38c3322bd75b8d707c0dcec2c9737ee.

---
+++ Assets/_Scripts/Enviroment/Entity/Player/Player.cs
@@ -0,0 +1,30 @@
+using roguelike.core.item;
+using roguelike.enviroment.ui.statemachine;
+using roguelike.system.input;
+
+namespace roguelike.enviroment.entity.player {
+    public class Player : Entity {
+
+        public UIStateMachine UIStateMachine { get; set; }
+        public PlayerInteractor PlayerInteractor { get; set; }
+        public PlayerInput PlayerInput { get; private set; }
+        public Inventory Inventory { get; private set; }
+
+        protected override void Awake() {
+            UIStateMachine = GetComponent<UIStateMachine>();
+
+            // input needs to be created first!!!!!
+            PlayerInput = new PlayerInput();
+            PlayerInput.Enable();
+
+            Inventory = new Inventory(this);
+            PlayerInteractor = new PlayerInteractor(this);
+
+            base.Awake();
+        }
+
+        protected override void Update() {
+            PlayerInteractor.UpdateInteractor();
+        }
+    }
+}
---
+++ Assets/_Scripts/Enviroment/item/EquipmentItem.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System.Linq;
+using roguelike.enviroment.entity.StatSystem;
+
+namespace roguelike.core.item {
+    public class EquipmentItem : Item {
+        public List<StatModifier> StatModifiers;
+
+        private EquipmentType _itemEquipmentType;
+
+        public EquipmentType ItemEquipmentType { get { return _itemEquipmentType; } }
+
+        public EquipmentItem(string id, EquipmentType type, params StatModifier[] modifiers) : base(id) {
+            _itemEquipmentType = type;
+            StatModifiers = modifiers.ToList();
+        }
+    }
+
+    public enum EquipmentType {
+        HELMET = 20,
+        CHESTPLATE = 21,
+        PANTS = 22,
+        BOOTS = 23,
+        MAIN_HAND = 24,
+        OFF_HAND = 25,
+        TRINKET = 26
+    }
+}
