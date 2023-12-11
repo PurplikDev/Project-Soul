a5857e32a90e7bbf49a3c47dd28a9384a4c0e016
Purplik <60230933+PurplikDev@users.noreply.github.com>
2023-12-11 12:37:27 +0100

    interactor stuff
    i honestly have no idea what i've done, i will just have to rewrite this thing AGAIN

--- Assets/_Scripts/enviroment/entity/player/PlayerInteractor.cs
+++ Assets/_Scripts/enviroment/entity/player/PlayerInteractor.cs
@@ -9,7 +9,11 @@ namespace roguelike.enviroment.entity.player {
 
         private Player _player;
         private PlayerInput _input;
-        private Vector3 _mousePos;
+        private Vector3 _mousePos, _aimPos;
+
+        public Transform speeeen;
+
+        private IHoverable _hoverable;
 
         // todo: re-add logic for IHoverable ( OnHoverEnter, OnHover, OnHoverExit )
 
@@ -17,21 +21,24 @@ namespace roguelike.enviroment.entity.player {
             _player = player;
             _input = player.PlayerInput;
 
+            speeeen = GameObject.Find("speeeen").transform;
+
             _input.EnviromentControls.PrimaryAction.started += Interact;
         }
 
         public void Interact(InputAction.CallbackContext context) {
-            
+            Debug.DrawLine(_player.Position, _aimPos, Color.white, 5f);
         }
 
         public void UpdateInteractor() {
-            var (hit, hitPos) = GetAimPos();
-            if(hit && Vector3.Distance(hitPos, _player.Position) < 2.5f)  {
-                Debug.DrawLine(_player.Position, hitPos);
-            }
+            var (hitPos, hoverable) = GetAimPos();
+            _aimPos = hitPos;
+            try {
+                HandleHoverEvents(hitPos, hoverable);
+            } catch(NullReferenceException) { /* :shrug: */}
         }
 
-        private (bool hit, Vector3 hitPos) GetAimPos() {
+        private (Vector3 hitPos, IHoverable hoverable) GetAimPos() {
             _mousePos = Mouse.current.position.ReadValue();
             Ray ray = Camera.main.ScreenPointToRay(_mousePos);
             RaycastHit hit;
@@ -39,15 +46,38 @@ namespace roguelike.enviroment.entity.player {
             if (Physics.Raycast(ray, out hit, Mathf.Infinity, LayerMask.GetMask("Entity", "Object"))) {
                 var hoverable = hit.transform.GetComponent<IHoverable>();
                 if (hoverable != null) {
-                    return (true, hit.transform.position);
+                    return (hit.transform.position, hoverable);
                 }
             }
 
             if (Physics.Raycast(ray, out hit, Mathf.Infinity, LayerMask.GetMask("Ground"))) {
-                return (true, new Vector3(hit.point.x, 1, hit.point.z));
+                return (new Vector3(hit.point.x, 1, hit.point.z), null);
             }
 
-            return (false, Vector3.zero);
+            return (Vector3.zero, null);
         }
-    }  
+#nullable enable
+        private void HandleHoverEvents(Vector3 hitPos, IHoverable? hoverable) {
+            if(Vector3.Distance(hitPos, _player.Position) < 2.5f) {
+                if(hoverable != null) {
+
+                    if(hoverable == _hoverable) {
+                        _hoverable.OnHover(_player);
+                    } else if(hoverable != _hoverable) {
+                        _hoverable?.OnHoverExit(_player);
+                        _hoverable = hoverable;
+                        _hoverable.OnHoverEnter(_player);
+                    }
+                } else {
+                    _hoverable.OnHoverExit(_player);
+                    _hoverable = null;
+                }
+
+            } else if(_hoverable != null) {
+                _hoverable.OnHoverExit(_player);
+                _hoverable = null;
+            }
+        }
+#nullable disable
+    }
 }
